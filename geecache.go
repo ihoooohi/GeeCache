0c03ee5824ef4537ec272eaf677578d470d1e424
ihoo <1506821833@qq.com>
2026-01-06 12:00:31 +0800

    Group

--- geecache.go
+++ geecache.go
@@ -1,5 +1,7 @@
 package geecache
 
+import "sync"
+
 //接口型函数
 type Getter interface {
 	Get(key string) ([]byte, error)
@@ -11,13 +13,38 @@ func (f GetterFunc) Get(key string) ([]byte, error){
 	return f(key)
 }
 
-func main() {
-	var g Getter = GetterFunc(func(key string) ([]byte, error) {
-
-	} )
-
-	g.Get(key)
+//Group结构
+type Group struct {
+	name string
+	getter Getter
+	mainCache cache
 }
 
+var(
+	mu sync.RWMutex
+	groups = make(map[string]*Group)
+)
 
-//Group结构
+func NewGroup(name string, getter Getter, mainCache cache) *Group {
+	if getter == nil {
+		panic("nil getter")
+	}
+
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name: name,
+		getter: getter,
+		mainCache: mainCache,
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mu.RLock()
+	defer mu.RUnlock()
+	g := groups[name]
+	return g
+}
+
