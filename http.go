f197baf2bafb18e01b174b566aac547ff587f568
Zuocan Ying <zuocan.ying@duke.edu>
2026-01-07 23:27:52 +0800

    http2

--- http.go
+++ http.go
@@ -1,10 +1,13 @@
 package geecache
 
-import(
+import (
+	"fmt"
+	"log"
 	"net/http"
+	"strings"
 )
 
-const defaultBasePath = "_geecache"
+const defaultBasePath = "/_geecache/"
 
 type HTTPPool struct {
 	self string //记录地址，包括ip/主机名 + 端口号
@@ -18,3 +21,40 @@ func NewHTTPPool(s string) *HTTPPool {
 	}
 }
 
+func (h *HTTPPool) Log(format string, v ...any) {
+	log.Printf("[Server %s] %s", h.self,fmt.Sprintf(format, v...) )
+
+}
+
+func (h *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, h.basePath) {
+		panic(fmt.Errorf("HTTPPool serving unexpected path: %s", r.URL.Path))
+	}
+	h.Log("%s, %s", r.Method, r.URL.Path)
+	//<basePath>/<group>/<key>
+	parts := strings.SplitN(r.URL.Path[len(h.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return //为什么要加return，因为http.Error不会停止程序，只会返回响应
+	}
+
+	groupName := parts[0]
+	key := parts[1]
+
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
+		return
+	}
+
+	value, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(value.ByteSlice())
+	
+
+}
