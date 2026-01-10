5147e52d6347d9526234228024c11b555b8bf33c
Zuocan Ying <zuocan.ying@duke.edu>
2026-01-11 00:40:24 +0800

    singleflight

---
+++ singleflight/singleflight.go
@@ -0,0 +1,24 @@
+package singleflight
+
+import "sync"
+
+type call struct {
+	wg sync.WaitGroup
+	value any
+	err error
+}
+
+type Group struct {
+	mu sync.Mutex
+	m map[string]*call
+}
+
+//Do函数的作用是判断这个请求是自己调用还是等待其他协程
+//为什么第二个参数是一个函数？这个函数是用来执行调用操作的，执行具体的业务逻辑用的，只是设计的时候为了拓展性设计成通用函数的样子，方便具体业务注入
+func (g *Group) Do(key string, fn func() (any, error)) (any, error){
+	g.mu.Lock()
+	//1.先判断等待情况
+
+	//2.再判断自己调用情况（第一个goroutine）
+}
+
